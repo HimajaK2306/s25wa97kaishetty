5c395ed1082356bc535b2e0888fca67ecba89702
Himaja2306 <himajakaishetty1@gmail.com>
2025-03-25 13:26:59 -0500

    new changes

--- routes/computation.js
+++ routes/computation.js
@@ -7,20 +7,18 @@ router.get('/', function(req, res, next) {
   let x = req.query.x ? parseFloat(req.query.x) : Math.random() * 10; // If x is passed as query, use it; otherwise, generate random value
   
   // Choose the function to apply based on the last digit of your ID (7)
-  const lastDigit = 7; // Since your last digit is 7, we will apply all three functions
+  // Since your last digit is 7, we will apply all three functions
 
-  // Apply all three functions in a sequence
-  let resultAsin = Math.asin(x);
-  let resultAsinh = Math.asinh(x);
-  let resultCos = Math.cos(x);
+  let resultAsin = Math.asin(x).toFixed(2);
+  let resultAsinh = Math.asinh(x).toFixed(2);
+  let resultCos = Math.cos(x).toFixed(2);
 
-  // Generate the response string
+  // Generate the response string with rounded results
   let responseMessage = `
     Math.asin(${x}) = ${resultAsin} <br>
     Math.asinh(${x}) = ${resultAsinh} <br>
     Math.cos(${x}) = ${resultCos}
   `;
-
   // Send the response with all three computations
   res.send(responseMessage);
 });
