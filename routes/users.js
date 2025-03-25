eb63513d503c0382630bea9ee16ff376cc8bb901
Himaja2306 <himajakaishetty1@gmail.com>
2025-03-25 13:04:06 -0500

    changes done

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+// Create the ratio and acc variables
+var ratio = 0.75;
+var acc = 1000;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  acc *= ratio; // Multiply acc by ratio
+  res.send('Value is: ${acc}'); // Send the result as "Value is: <calculated_value>"
 });
 
 module.exports = router;
+
